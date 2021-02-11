2dcc0072a2ddf10b03db1fe997309d22f2bb508a
MaxCarlson <bart4282819@gmail.com>
2021-02-10 19:47:38 -0800

    Classification written but not working

--- NaiveBayes/NaiveBayes.py
+++ NaiveBayes/NaiveBayes.py
@@ -1,6 +1,6 @@
 import csv
 import numpy as np
-
+import math
 
 
 
@@ -9,33 +9,55 @@ class NaiveBayes:
     def __init__(self, path):
         data = np.genfromtxt(path, dtype=float, delimiter=',')
 
+        # Split into spam/not spam
         spam    = data[0:1813]
         notSpam = data[1813:]
         np.random.shuffle(spam)
         np.random.shuffle(notSpam)
         
-        self.spamPrior = 1813 / len(data)
-        self.notSpamPrior = len(notSpam) / len(data)
+        # Calcualte Priors
+        self.priors = [len(notSpam) / len(data), 1813 / len(data)]
+        self.priors = [math.log(x) for x in self.priors]
+        
+        # Used for calculating means and stds for model
+        self.trainingData = [notSpam[0:int(len(notSpam)/2)], spam[0:int(len(spam)/2)]]
 
-        self.training = notSpam[0:int(len(notSpam)/2)]
-        self.test     = notSpam[int(len(notSpam)/2):]
+        # Calculate means and stds
+        self.stats()
 
-        self.training = np.vstack([self.training, spam[0:int(len(spam)/2)]])
-        self.test     = np.vstack([self.test, spam[int(len(spam)/2):]])
+        # Split data
+        self.training = np.vstack([self.trainingData[0], self.trainingData[1]])
+        self.test     = np.vstack([notSpam[int(len(notSpam)/2):], spam[int(len(spam)/2):]])
 
         np.random.shuffle(self.training)
         np.random.shuffle(self.test)
 
-        self.stats()
+        self.classify(self.test[:,:-1], self.test[:,-1:])
+
 
     def stats(self):
 
-        # stddv & mean of columns
-        self.std     = np.std(self.training[:,:-1], axis=0)
-        self.std     = np.where(self.std==0, 0.0001, self.std)
-        self.mean    = np.mean(self.training[:,:-1], axis=0)
+        self.mean   = [np.mean(x[:,:-1], axis=0) for x in self.trainingData]
+        self.std    = [np.std(x[:,:-1], axis=0)  for x in self.trainingData]
+        self.std    = [np.where(x==0.0, 0.0001, x) for x in self.std]
 
-    def prob(self, )
+        # stddv & mean of columns
+        #self.std     = np.std(self.training[:,:-1], axis=0)
+        #self.std     = np.where(self.std==0, 0.0001, self.std)
+        #self.mean    = np.mean(self.training[:,:-1], axis=0)
+
+    def classify(self, input, labels):
+        norm = [1.0 / ((math.sqrt(2 * math.pi) * self.std[i])) for i in range(2)]
+        power = [-(np.square(input - self.mean[i]) / 2 * np.square(self.std[i])) 
+                 for i in range(2)]
+
+        likelihoods = [np.log(norm[i] * np.exp(power[i])) for i in range(2)]
+
+        preds = [self.priors[i] + np.sum(likelihoods[i], axis=1) for i in range(2)]
+
+        a=5
+
+    #def prob(self, )
 
 
 
