63f624ae55debb0122f6a063c32d4c947040aed9
MaxCarlson <bart4282819@gmail.com>
2021-02-13 18:23:02 -0800

    Fixed the bug, was parens

--- NaiveBayes/NaiveBayes.py
+++ NaiveBayes/NaiveBayes.py
@@ -12,15 +12,20 @@ class NaiveBayes:
         # Split into spam/not spam
         spam    = data[0:1813]
         notSpam = data[1813:]
+        np.random.seed(1)
         np.random.shuffle(spam)
         np.random.shuffle(notSpam)
         
-        # Calcualte Priors
-        self.priors = [len(notSpam) / len(data), 1813 / len(data)]
-        self.priors = [math.log(x) for x in self.priors]
         
         # Used for calculating means and stds for model
         self.trainingData = [notSpam[0:int(len(notSpam)/2)], spam[0:int(len(spam)/2)]]
+        trainingCount = len(self.trainingData[0]) + len(self.trainingData[1])
+
+        # TODO: Calculate Priors from training ONLY!!!
+        # Calcualte Priors
+        self.priors = [len(self.trainingData[0]) / trainingCount, 
+                       len(self.trainingData[1]) / trainingCount]
+        self.priors = [math.log(x) for x in self.priors]
 
         # Calculate means and stds
         self.stats()
@@ -41,19 +46,21 @@ class NaiveBayes:
         self.std    = [np.std(x[:,:-1], axis=0)  for x in self.trainingData]
         self.std    = [np.where(x==0.0, 0.0001, x) for x in self.std]
 
-        # stddv & mean of columns
-        #self.std     = np.std(self.training[:,:-1], axis=0)
-        #self.std     = np.where(self.std==0, 0.0001, self.std)
-        #self.mean    = np.mean(self.training[:,:-1], axis=0)
-
     def classify(self, input, labels):
-        norm = [1.0 / ((math.sqrt(2 * math.pi) * self.std[i])) for i in range(2)]
-        power = [-(np.square(input - self.mean[i]) / 2 * np.square(self.std[i])) 
+        norm = [1 / (math.sqrt(2 * math.pi) * self.std[i]) for i in range(2)]
+        power = [(np.square(input - self.mean[i]) / (2 * np.square(self.std[i]))) 
                  for i in range(2)]
 
-        likelihoods = [np.log(norm[i] * np.exp(power[i])) for i in range(2)]
+        #t = np.exp(power[0])
+
+        likelihoods = [np.log(np.exp(-power[i]) + 10e-10) * norm[i] for i in range(2)]
 
         preds = [self.priors[i] + np.sum(likelihoods[i], axis=1) for i in range(2)]
+        preds = [0 if x >= v else 1 for x, v in zip(preds[0], preds[1])]
+
+        labels  = np.reshape(labels, (len(labels)))
+        results = (labels == preds) * 1
+        accuracy = np.sum(results) / len(results)
 
         a=5
 
